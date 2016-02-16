e197d25d573d2f4294c849c6f4f6b93495db8020
Fabio Napoleoni <f.napoleoni@gmail.com>
2016-02-16 16:38:53 +0100

    Using TCP, raised timeouts

--- features/step_definitions/integration.rb
+++ features/step_definitions/integration.rb
@@ -1,13 +1,13 @@
 Given(/^a configured OpenVPN instance with "([^"]*)"$/) do |flags|
   # Configure the server using a clean data volume and detected ip address
-  run_simple %Q{docker run --volumes-from #{@data_volume} --rm kylemanna/openvpn ovpn_genconfig -u udp://ovpn-server #{flags}}
+  run_simple %Q{docker run --volumes-from #{@data_volume} --rm kylemanna/openvpn ovpn_genconfig -u tcp://ovpn-server #{flags}}
   # Build a passwordless PKI, it may take long time to complete. Give it 2 minutes to complete
   run_simple %Q{docker run --volumes-from #{@data_volume} --rm -e "EASYRSA_BATCH=1" kylemanna/openvpn ovpn_initpki nopass}, exit_timeout: 120
 end
 
 Given(/^a running OpenVPN process$/) do
   # This will run the server image and immediately detach.
-  run %Q{docker run --volumes-from #{@data_volume} --name ovpn-server --rm -p 1194:1194 --privileged kylemanna/openvpn}, startup_wait_time: 5
+  run %Q{docker run --volumes-from #{@data_volume} --name ovpn-server --rm -p 1194:1194 --privileged kylemanna/openvpn}, startup_wait_time: 10
   # With this variable running server can be accessed later
   @running_server = last_command_started
 end
@@ -29,10 +29,8 @@ When(/^I run "([^"]*)" openvpn client in docker$/) do |client_name|
   cd 'clients' do
     # Use container linking to make client connection working, mount client config volume and rm it when done
     docker_options = %Q{--rm --privileged --name #{client_name} --link ovpn-server:ovpn-server --volume #{Dir.getwd}:/clients}
-    # Run client daemon in background and return control to process, wait 5 seconds before returing to give time to docker to start
-    run %Q{docker run #{docker_options} kylemanna/openvpn #{openvpn_client}}, startup_wait_time: 5
-    # Wait for connection established in VPN
-    step %q{I wait for output to contain "Initialization Sequence Completed"}
+    # Run client daemon in background and return control to process, wait 15 seconds before returing to give time to docker to start
+    run %Q{docker run #{docker_options} kylemanna/openvpn #{openvpn_client}}, startup_wait_time: 15
   end
   # Save client command instance to retrieve it later
   @clients[client_name.to_sym] = last_command_started
